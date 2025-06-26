f5e5d3f2778018494b2052d9b892822507ca12ef
Lambourne2 <140791275+Lambourne2@users.noreply.github.com>
2025-06-26 02:40:18 -0600

    Fix: Update build with correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/llm-course-website/', // Add this line for GitHub Pages deployment
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
